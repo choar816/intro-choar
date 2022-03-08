2399f9ac85f7a91641882bafda5e23f28d70bd4d
Ahra Cho <choar816@postech.ac.kr>
2022-03-08 22:23:50 +0900

    Set up: React development environment without CRA

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Hi!</h1>
+      <h2>Intro of choar</h2>
+    </div>
+  );
+};
+  
+export default App;
