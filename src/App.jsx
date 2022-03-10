027c430ffaa6819e9d4af7673156143fe6e06745
Ahra Cho <choar816@postech.ac.kr>
2022-03-10 16:54:22 +0900

    Set up: ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="container">
       <h1>Hi!</h1>
       <h2>Intro of choar</h2>
     </div>
   );
-};
-  
+}
+
 export default App;
