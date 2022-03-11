d7b427981e34de20fe6defa1ff9f20abf2fc9697
Ahra Cho <choar816@postech.ac.kr>
2022-03-11 15:51:48 +0900

    Add: Resume - about me part

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+const contactList = {
+  GitHub: 'https://github.com/choar816',
+  LinkedIn: 'https://www.linkedin.com/in/ahra-cho-209b6b187/',
+  Mail: 'mailto:ahracho816@gmail.com',
+};
+
 function Resume() {
   return (
-    <div>Resume</div>
+    <div>
+      <section className="about-me">
+        <h2>📌 ABOUT ME</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          culpa odio beatae in minus nam necessitatibus dolores rerum, deserunt
+          labore dignissimos nemo, distinctio quae temporibus deleniti fuga
+          molestiae. Voluptatibus, laudantium.
+        </p>
+        <ul className="list-contact">
+          {Object.keys(contactList).map((val) => <li key={val}><a target="_blank" rel="noreferrer" href={contactList[val]}>{val}</a></li>)}
+        </ul>
+      </section>
+    </div>
   );
 }
 
